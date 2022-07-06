4b2fad24364227a12d5974ddc2d0712fc9b69e82
cokeIII <coke2484@gmail.com>
2022-07-06 21:53:19 +0700

    f

--- checkEnroll.php
+++ checkEnroll.php
@@ -1,7 +1,6 @@
 <?php
 require_once("connect.php");
-session_start();
-$student_id = $_SESSION["student_id"];
+$student_id = $_POST["student_id"];
 $sql = "select * from enroll where student_id = '$student_id'";
 $res = mysqli_query($conn, $sql);
 $numRow = mysqli_num_rows($res);

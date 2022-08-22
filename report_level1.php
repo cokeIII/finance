a89cbfd2f6017413094e9589fd01fe36ed91dd8b
cokeIII <coke2484@gmail.com>
2022-08-22 13:22:42 +0700

    f

--- report_level1.php
+++ report_level1.php
@@ -92,7 +92,7 @@ function countNot($g_id)
 function countYes_nopass($g_id)
 {
     global $conn;
-    echo $sql = "
+    $sql = "
         select count(e.student_id) as std_all from enroll e
         inner join documents d on d.student_id = e.student_id  
         where group_id = '$g_id' and d.status = 'เอกสารไม่ถูกต้องสมบูรณ์' 

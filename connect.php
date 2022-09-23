842a796d7d5e2b52686d2b58d4787dd02cd87f9d
cokeIII <52317579+cokeIII@users.noreply.github.com>
2022-09-23 07:19:42 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "chontech2020!";
+$password = "";
 $database = "finance";
 // Create connection
 $conn = new mysqli($servername, $username, $password,$database);
